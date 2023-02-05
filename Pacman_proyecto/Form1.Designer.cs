4d664527917d5af9651a5b13106f5f82d97fbc1d
ximenavm7 <122324007+ximenavm7@users.noreply.github.com>
2023-02-05 15:12:54 -0600

    Primer versión con movimiento de pacman

---
+++ Pacman_proyecto/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace pacman_proyecto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(151, 197);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(198, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Pacman_proyecto/Form1.cs
@@ -0,0 +1,138 @@
+﻿using Pacman_proyecto;
+using Pacman_proyecto.Recursos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pacman_proyecto
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        static Graphics g;
+        private Point pacmanPos;
+        public enum direction { Left, Right, Up, Down };
+        int playerX, playerY;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(395, 395);
+            g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+
+            DrawMap();
+        }
+
+        private void DrawMap()
+        {
+            Graphics g = Graphics.FromImage(bmp);
+            g.Clear(Color.Black);
+            pacmanPos = new Point();
+
+            for (int y = 0; y < Mapas.map0.GetLength(0); y++)
+            {
+                for (int x = 0; x < Mapas.map0.GetLength(1); x++)
+                {
+                    if (Mapas.map0[y, x] == 0)
+                    {
+                        g.FillRectangle(new SolidBrush(Color.FromArgb(35, 35, 35)), x * 20, y * 20, 10, 10);
+                    }
+                    if (Mapas.map0[y, x] == 1)
+                    {
+                        g.FillRectangle(new SolidBrush(Color.Green), x * 20, y * 20, 10, 10);
+                    }
+                    else if (Mapas.map0[y, x] == 5)
+                    {
+                        playerX = x; playerY = y;
+                        pacmanPos.X = 198 + (x * 20);
+                        pacmanPos.Y = 12 + (y * 20);
+                        pictureBox2.Location = pacmanPos;
+                        //pictureBox2.Image = Resource1.pacman_foto;
+                        //*/
+                       g.FillRectangle(new SolidBrush(Color.Yellow), x * 20, y * 20, 10, 10);
+                        g.DrawRectangle(Pens.DarkOrange, x * 20, y * 20, 10, 10);
+                    }
+                    else if (Mapas.map0[y, x] == 2)
+                    {
+                        g.DrawRectangle(Pens.Purple, x * 20, y * 20, 10, 10);
+                    }
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //MessageBox.Show("you pressed a key");   
+            if(e.KeyCode.Equals(Keys.Up))
+            {
+                if (Mapas.map0[playerY-1, playerX] != 2)
+                {
+                    if (Mapas.map0[playerY-1, playerX] == 1)
+                        Mapas.map0[playerY, playerX] = 0;
+                    playerY--;
+                    Mapas.map0[playerY, playerX] = 5;
+                    Mapas.map0[playerY + 1, playerX] = 0;
+                    Invalidate();
+                    DrawMap();
+                }
+            }
+            if (e.KeyCode.Equals(Keys.Down))
+            {
+                if (Mapas.map0[playerY + 1, playerX] != 2)
+                {
+                    if (Mapas.map0[playerY + 1, playerX] == 1)
+                        Mapas.map0[playerY, playerX] = 0;
+                    playerY++;
+                    Mapas.map0[playerY, playerX] = 5;
+                    Mapas.map0[playerY - 1, playerX] = 0;
+                    Invalidate();
+                    DrawMap();
+                }
+            }
+            if (e.KeyCode.Equals(Keys.Right))
+            {
+                if (Mapas.map0[playerY, playerX + 1] != 2)
+                {
+                    if (Mapas.map0[playerY, playerX + 1] == 1)
+                        Mapas.map0[playerY, playerX] = 0;
+                    playerX++;
+                    Mapas.map0[playerY, playerX] = 5;
+                    Mapas.map0[playerY, playerX - 1] = 0;
+                    Invalidate();
+                    DrawMap();
+                }
+            }
+            if (e.KeyCode.Equals(Keys.Left))
+            {
+                if (Mapas.map0[playerY, playerX - 1] != 2)
+                {
+                    if (Mapas.map0[playerY, playerX - 1] == 1)
+                        Mapas.map0[playerY, playerX] = 0;
+                    playerX--;
+                    Mapas.map0[playerY, playerX] = 5;
+                    Mapas.map0[playerY, playerX + 1] = 0;
+                    Invalidate();
+                    DrawMap();
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+        }
+    }
+}
