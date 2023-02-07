43c431f7cc29dd220e51d53003f45247ce438699
ximenavm7 <122324007+ximenavm7@users.noreply.github.com>
2023-02-06 18:07:22 -0600

    Add files via upload
    Faltan fantasmas del pacman

--- Pacman_proyecto/Form1.Designer.cs
+++ Pacman_proyecto/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -40,10 +41,22 @@
             // timer1
             // 
             this.timer1.Enabled = true;
+            this.timer1.Interval = 135;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
             // 
             // pictureBox2
             // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(151, 197);
             this.pictureBox2.Name = "pictureBox2";
@@ -55,9 +68,9 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Location = new System.Drawing.Point(198, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(191, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.Size = new System.Drawing.Size(420, 420);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -66,6 +79,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
@@ -75,6 +89,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -83,6 +98,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Pacman_proyecto/Form1.cs
+++ Pacman_proyecto/Form1.cs
@@ -18,121 +18,217 @@ namespace pacman_proyecto
         Bitmap bmp;
         static Graphics g;
         private Point pacmanPos;
-        public enum direction { Left, Right, Up, Down };
-        int playerX, playerY;
+        public int playerX, playerY;
+        public int counter = 0;
+        bool right, left, up, down, hold = true;
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            
+            if (counter < 20)
+            {
+                if (right)
+                {
+                    Movement(Mapas.map1, playerX + 1, playerY);
+                    DrawMap(Mapas.map1);
+                }
+
+                else if (left)
+                {
+                    Movement(Mapas.map1, playerX - 1, playerY);
+                    DrawMap(Mapas.map1);
+                }
+
+                else if (up)
+                {
+                    Movement(Mapas.map1, playerX, playerY - 1);
+                    DrawMap(Mapas.map1);
+                }
+
+                else if (down)
+                {
+                    Movement(Mapas.map1, playerX, playerY + 1);
+                    DrawMap(Mapas.map1);
+                }
+
+            }
+            if (counter >= 20 && counter < 40)
+            {
+                if (right)
+                {
+                    Movement(Mapas.map2, playerX + 1, playerY);
+                    DrawMap(Mapas.map2);
+                }
+
+                else if (left)
+                {
+                    Movement(Mapas.map2, playerX - 1, playerY);
+                    DrawMap(Mapas.map2);
+                }
+
+                else if (up)
+                {
+                    Movement(Mapas.map2, playerX, playerY - 1);
+                    DrawMap(Mapas.map2);
+                }
+
+                else if (down)
+                {
+                    Movement(Mapas.map2, playerX, playerY + 1);
+                    DrawMap(Mapas.map2);
+                }
+            }
         }
 
         public Form1()
         {
             InitializeComponent();
-            bmp = new Bitmap(395, 395);
+            bmp = new Bitmap(420, 420);
             g = Graphics.FromImage(bmp);
             pictureBox1.Image = bmp;
 
-            DrawMap();
+            DrawMap(Mapas.map1);            
+            label1.Text = counter.ToString();
+
         }
 
-        private void DrawMap()
-        {
+        private void DrawMap(int[,] map) 
+        {    
             Graphics g = Graphics.FromImage(bmp);
             g.Clear(Color.Black);
             pacmanPos = new Point();
 
-            for (int y = 0; y < Mapas.map0.GetLength(0); y++)
+            for (int y = 0; y < map.GetLength(0); y++)
             {
-                for (int x = 0; x < Mapas.map0.GetLength(1); x++)
+                for (int x = 0; x < map.GetLength(1); x++)
                 {
-                    if (Mapas.map0[y, x] == 0)
+                    if (map[y, x] == 0) // Blank space
                     {
-                        g.FillRectangle(new SolidBrush(Color.FromArgb(35, 35, 35)), x * 20, y * 20, 10, 10);
+                        g.FillRectangle(new SolidBrush(Color.FromArgb(0, 0, 0)), x * 20, y * 20, 10, 10);
                     }
-                    if (Mapas.map0[y, x] == 1)
+                    else if (map[y, x] == 1) // Coins
                     {
-                        g.FillRectangle(new SolidBrush(Color.Green), x * 20, y * 20, 10, 10);
+                        g.FillEllipse(new SolidBrush(Color.Gray), (x * 20)+5, (y * 20)+5, 7, 7);
                     }
-                    else if (Mapas.map0[y, x] == 5)
+                    else if (map[y, x] == 2) // Wall
+                    {
+                        if (counter < 20)
+                        {
+                            g.FillRectangle(new SolidBrush(Color.BlueViolet), (x * 20) + 1, (y * 20) + 1, 18, 18);
+                            g.DrawRectangle(Pens.Purple, x * 20, y * 20, 20, 20);
+                        }
+                        else if (counter >= 20 && counter < 40)
+                        {
+                            g.FillRectangle(new SolidBrush(Color.IndianRed), (x * 20) + 1, (y * 20) + 1, 18, 18);
+                            g.DrawRectangle(Pens.DarkRed, x * 20, y * 20, 20, 20);
+                        }
+                    }
+                    else if (map[y, x] == 3) // Door
+                    {
+                        g.DrawRectangle(Pens.Gray, x * 20, y * 20, 20, 2);
+                    }
+                    else if (map[y, x] == 5) // Pacman
                     {
                         playerX = x; playerY = y;
-                        pacmanPos.X = 198 + (x * 20);
+                        pacmanPos.X = 191 + (x * 20);
                         pacmanPos.Y = 12 + (y * 20);
                         pictureBox2.Location = pacmanPos;
-                        //pictureBox2.Image = Resource1.pacman_foto;
-                        //*/
-                       g.FillRectangle(new SolidBrush(Color.Yellow), x * 20, y * 20, 10, 10);
-                        g.DrawRectangle(Pens.DarkOrange, x * 20, y * 20, 10, 10);
+                        //g.FillRectangle(new SolidBrush(Color.Yellow), x * 20, y * 20, 10, 10);
+                        //g.DrawRectangle(Pens.DarkOrange, x * 20, y * 20, 10, 10);
                     }
-                    else if (Mapas.map0[y, x] == 2)
+                    else if (map[y, x] == 6) // Portal 1
                     {
-                        g.DrawRectangle(Pens.Purple, x * 20, y * 20, 10, 10);
+                        g.DrawEllipse(Pens.Blue, x * 20, y * 20, 15, 20);
+                    }
+                    else if (map[y, x] == 7) // Portal 2
+                    {
+                        g.DrawEllipse(Pens.OrangeRed, x * 20, y * 20, 15, 20);
                     }
                 }
+            }
+        }
+
+        private void Movement(int [,] map, int x, int y)
+        {
+            if (map[y, x] == 6) // Portal 1
+            {
+                playerX = 19;
+                map[playerY, playerX] = 5;
+                map[y, x + 1] = 0;
+            }
+            else if (map[y, x] == 7) // Portal 2
+            {
+                playerX = 1;
+                map[playerY, playerX] = 5;
+                map[y, x - 1] = 0;
+            }
+            else if (map[y, x] != 2 && map[y, x] != 3) // Cannot go through wall
+            {
+                if (map[y, x] == 1) // Finds a coin
+                {
+                    map[y, x] = 0;
+                    counter++;
+                    label1.Text = (counter * 100).ToString();
+                }
+                map[playerY, playerX] = 0;
+                playerY = y;
+                playerX = x;
+                map[playerY, playerX] = 5;
+                Invalidate();
+                //DrawMap(map);
             }
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            //MessageBox.Show("you pressed a key");   
-            if(e.KeyCode.Equals(Keys.Up))
+            if (e.KeyData == Keys.Right & hold)
             {
-                if (Mapas.map0[playerY-1, playerX] != 2)
-                {
-                    if (Mapas.map0[playerY-1, playerX] == 1)
-                        Mapas.map0[playerY, playerX] = 0;
-                    playerY--;
-                    Mapas.map0[playerY, playerX] = 5;
-                    Mapas.map0[playerY + 1, playerX] = 0;
-                    Invalidate();
-                    DrawMap();
-                }
+                right = true;
+                hold = false;
+                pictureBox2.Image = Resource1.PacmanR;
+                
             }
-            if (e.KeyCode.Equals(Keys.Down))
+            else if (e.KeyData == Keys.Left & hold)
             {
-                if (Mapas.map0[playerY + 1, playerX] != 2)
-                {
-                    if (Mapas.map0[playerY + 1, playerX] == 1)
-                        Mapas.map0[playerY, playerX] = 0;
-                    playerY++;
-                    Mapas.map0[playerY, playerX] = 5;
-                    Mapas.map0[playerY - 1, playerX] = 0;
-                    Invalidate();
-                    DrawMap();
-                }
+                left = true;
+                hold = false;
+                pictureBox2.Image = Resource1.PacmanL;
             }
-            if (e.KeyCode.Equals(Keys.Right))
+            else if (e.KeyData == Keys.Up & hold)
             {
-                if (Mapas.map0[playerY, playerX + 1] != 2)
-                {
-                    if (Mapas.map0[playerY, playerX + 1] == 1)
-                        Mapas.map0[playerY, playerX] = 0;
-                    playerX++;
-                    Mapas.map0[playerY, playerX] = 5;
-                    Mapas.map0[playerY, playerX - 1] = 0;
-                    Invalidate();
-                    DrawMap();
-                }
+                up = true;
+                hold = false;
+                pictureBox2.Image = Resource1.PacmanU;
             }
-            if (e.KeyCode.Equals(Keys.Left))
+            else if (e.KeyData == Keys.Down & hold)
             {
-                if (Mapas.map0[playerY, playerX - 1] != 2)
-                {
-                    if (Mapas.map0[playerY, playerX - 1] == 1)
-                        Mapas.map0[playerY, playerX] = 0;
-                    playerX--;
-                    Mapas.map0[playerY, playerX] = 5;
-                    Mapas.map0[playerY, playerX + 1] = 0;
-                    Invalidate();
-                    DrawMap();
-                }
+                down = true;
+                hold = false;
+                pictureBox2.Image = Resource1.PacmanD;
             }
         }
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-            
+            if (e.KeyData == Keys.Right & !hold)
+            {
+                right = false;
+                hold = true;
+            }
+            else if (e.KeyData == Keys.Left & !hold)
+            {
+                left = false;
+                hold = true;
+            }
+            else if (e.KeyData == Keys.Up & !hold)
+            {
+                up = false;
+                hold = true;
+            }
+            else if (e.KeyData == Keys.Down & !hold)
+            {
+                down = false;
+                hold = true;
+            }
         }
     }
 }
